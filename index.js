b2f372e206d21a22d9821581145bbde4a755a965
Mickael BOLNET <mickael.bolnet@gmail.com>
2018-08-21 21:37:33 +0200

    ok

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ app.use(express.static('public'));
 
 // our first Route
 app.get('/', (request, response, next) => {
-  // response.send('<html><h1>Welcome Mike. :)</h1><script>alert("Hello");</script></html>');
-  response.sendFile(__dirname + '/views/index.html');
+	console.log(request);
+	let name = request.query.name;
+	console.log(request.query.name);
+	response.send(`<html><h1>Welcome ${name}. :)</h1><script>alert("Hello");</script></html>`);
+  // response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/home', (request, response, next) => {
-  // response.send('<html><h1>Welcome Mike. :)</h1><script>alert("Hello");</script></html>');
-  response.sendFile(__dirname + '/views/index.html');
-});
+app.get('/about', (request, response, next) => {
+	response.sendFile(__dirname + '/views/about.html');
+})
 
 // Server Started
 app.listen(3000, () => {
