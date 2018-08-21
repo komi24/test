fab065fd25510cebeb4941b49de62fe12ae7acfc
Mickael BOLNET <mickael.bolnet@gmail.com>
2018-08-21 18:44:43 +0200

    index change

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get('/', (request, response, next) => {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/home', (request, response, next) => {
+  // response.send('<html><h1>Welcome Mike. :)</h1><script>alert("Hello");</script></html>');
+  response.sendFile(__dirname + '/views/index.html');
+});
+
 // Server Started
 app.listen(3000, () => {
   console.log('My first app listening on port 3000!');
